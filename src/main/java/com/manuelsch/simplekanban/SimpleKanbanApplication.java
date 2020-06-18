8c64954868ddc63997704822bd5e99dbf4aceb19
Manuel <e1426298@student.tuwien.ac.at>
2020-06-19 01:01:04 +0200

    initial commit

---
+++ src/main/java/com/manuelsch/simplekanban/SimpleKanbanApplication.java
@@ -0,0 +1,13 @@
+package com.manuelsch.simplekanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleKanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleKanbanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manuelsch/simplekanban/SimpleKanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuelsch.simplekanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleKanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
