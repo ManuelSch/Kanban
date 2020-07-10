57a8d3dcf79c9d2ef5f56e6aec24be7cf6c90354
Manuel <e1426298@student.tuwien.ac.at>
2020-07-10 22:14:12 +0200

    BoardController.createBoard() now uses SuccessResponse wrapper class, validation and Exceptions

--- src/main/java/com/manuelsch/simplekanban/controllers/BoardController.java
+++ src/main/java/com/manuelsch/simplekanban/controllers/BoardController.java
@@ -1,24 +1,36 @@
 package com.manuelsch.simplekanban.controllers;
 
+import com.manuelsch.simplekanban.DTOs.SuccessResponse;
+import com.manuelsch.simplekanban.DTOs.board.CreateBoardRequest;
+import com.manuelsch.simplekanban.DTOs.board.CreateBoardResponse;
+import com.manuelsch.simplekanban.DTOs.exceptionHandling.InputValidationException;
 import com.manuelsch.simplekanban.models.Board;
 import com.manuelsch.simplekanban.service.BoardService;
+import com.manuelsch.simplekanban.DTOs.exceptionHandling.RecordNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * REST API endpoint for Kanban boards
+ */
 @RestController
 @RequestMapping("/api/board")
 public class BoardController {
 
     BoardService boardService;
 
+    @Autowired
     public BoardController(BoardService boardService) {
         this.boardService = boardService;
     }
 
     @PostMapping
-    public Board createBoard(@RequestBody Board newBoard) {
-        return boardService.createBoard(newBoard);
+    public SuccessResponse<CreateBoardResponse> createBoard(@RequestBody CreateBoardRequest newBoard) throws InputValidationException, RecordNotFoundException {
+        newBoard.validate();
+        Board createdBoard = boardService.createBoard(newBoard.getTitle());
+        return new SuccessResponse<>(new CreateBoardResponse(createdBoard));
     }
 
     @GetMapping
