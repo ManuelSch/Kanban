571ac381e7698703fbdb9042d7cecd869e1b72cc
Manuel <e1426298@student.tuwien.ac.at>
2020-07-15 19:33:05 +0200

    backend: disabled CSRF protection

--- backend/src/main/java/com/manuelsch/simplekanban/WebSecurityConfig.java
+++ backend/src/main/java/com/manuelsch/simplekanban/WebSecurityConfig.java
@@ -1,16 +1,21 @@
 package com.manuelsch.simplekanban;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+        http
+                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+                .and().csrf().disable()     // simply disabled CSRF protection for now
+        ;
     }
 
 }
