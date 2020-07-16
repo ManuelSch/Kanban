00d48dec110f1ec8c2de7862898ef081057eb357
Manuel <e1426298@student.tuwien.ac.at>
2020-07-16 21:05:27 +0200

    frontend: rudimentary BoardComponent implementation

--- frontend/src/main/web/src/app/components/board/board.component.ts
+++ frontend/src/main/web/src/app/components/board/board.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BoardService } from '../../services/board.service';
 import { Board } from '../../models/Board';
+import { BoardColumnService } from '../../services/board-column.service';
 
 @Component({
   selector: 'app-board',
@@ -12,11 +13,21 @@ export class BoardComponent implements OnInit {
 
   board?: Board;
 
-  constructor(private route: ActivatedRoute, private boardService: BoardService) {
+  constructor(private route: ActivatedRoute, private router: Router, private boardService: BoardService, private boardColumnService: BoardColumnService) {
   }
 
   async ngOnInit() {
-    this.board = await this.boardService.getBoardById(this.route.snapshot.params['boardId']);
+    try {
+      this.board = await this.boardService.getBoardById(this.route.snapshot.params['boardId']);
+      this.board.columns = this.board.columns.sort((a, b) => a.position - b.position);
+    }
+    catch (e) {
+      await this.router.navigateByUrl('/');
+    }
+  }
+
+  async createNewColumn() {
+    this.board = await this.boardColumnService.createBoardColumn('New Kanban column', this.board.id);
   }
 
 }
